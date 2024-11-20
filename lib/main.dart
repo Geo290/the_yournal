630c3438c4d27485d9cc271e3e04d582779bed69
Geo290 <jorgeemmanuel.2003@gmail.com>
2024-11-20 14:34:06 -0600

    Refactor: main screen set to Signin Screen; navigation between pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:yournal/presentation/screens/signin/signin_screen.dart';
-import 'package:yournal/presentation/screens/signup/contact_info_screen.dart';
-import 'package:yournal/presentation/screens/signup/personal_info_screen.dart';
 import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +17,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ContactInfoScreen()
+      home: SigninScreen()
     );
   }
 }
